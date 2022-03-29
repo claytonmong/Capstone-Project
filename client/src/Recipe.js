4335bfab11e2da107f906d7811b7044b13cdfcb6
Clayton Mong <mong.36@osu.edu>
2022-03-29 16:27:58 -0400

    added back button

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -3,17 +3,30 @@
 
 import RecipeInfo from "./recipe-components/RecipeInfo";
 
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 const Recipe = () => {
+	const navigate = useNavigate();
 	const location = useLocation();
 	const recipe = location.state.item;
+	//const searchResutls = location.state.searchresults;
 	
+	const handleClick = () => {
+		console.log("clicked");
+		navigate("/", { state: { id: 1, item: recipe } });
+	}
 	console.log(location.state.item)
 	  return (
 	<div>
 	  <RecipeInfo recipe={recipe}/>
+	  <input
+		type="submit"
+		value="View Full Recipe"
+		className="btn btn-block"
+		onClick={handleClick}
+	  />
 	</div>
+	
   );
 };
 export default Recipe;
