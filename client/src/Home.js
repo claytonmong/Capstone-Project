6c515725453a7867f881f494f6b679de739df138
[MMusa39] <[musa.39@osu.edu]>
2022-03-29 16:41:37 -0400

    fixed issue where whitespace ingredients crashed app

--- client/src/Home.js
+++ client/src/Home.js
@@ -66,7 +66,7 @@ const Home = (props) => {
     let arr = "";
     for (let ingredient in ingredients) {
       arr += ingredients[ingredient].text + ",";
-      //console.log(ingredients[ingredient].text)
+      //console.log(ingredients[ingredient].text);
     }
     // slice the last if it is ,
     if (arr.charAt(arr.length - 1) === ",") {
@@ -84,30 +84,36 @@ const Home = (props) => {
     }
     //navigate(`/search?q=${arr}`);
     //console.log(typeof arr)
-    console.log(arr);
-    fetch("/search", {
-      method: "POST",
-      body: JSON.stringify({
-        content: arr,
-      }),
-    })
-      .then((response) => response.json().then((data) => ({ data, response })))
-      .then(({ data, response }) => {
-        //console.log(data["results"]["hits"]["hits"]);
-        for (let hit in data["results"]["hits"]["hits"]) {
-          let h = JSON.stringify(
-            data["results"]["hits"]["hits"][hit]["_source"]
-          );
+    if (arr.trim().length === 1) {
+      r.push("No results found");
+    } else {
+      console.log(arr);
+      fetch("/search", {
+        method: "POST",
+        body: JSON.stringify({
+          content: arr,
+        }),
+      })
+        .then((response) =>
+          response.json().then((data) => ({ data, response }))
+        )
+        .then(({ data, response }) => {
+          //console.log(data["results"]["hits"]["hits"]);
+          for (let hit in data["results"]["hits"]["hits"]) {
+            let h = JSON.stringify(
+              data["results"]["hits"]["hits"][hit]["_source"]
+            );
 
-          r.push(h);
-        }
-        if (r.length === 0) {
-          r.push("No results found");
-        }
-        response = data;
-        setResults(r);
-        console.log(results);
-      });
+            r.push(h);
+          }
+          if (r.length === 0) {
+            r.push("No results found");
+          }
+          response = data;
+          setResults(r);
+          console.log(results);
+        });
+    }
     console.log(results);
     console.log(typeof SampleRecipeData[0]);
     // console.log(typeof JSON.parse(results[0]))
