69e356c7d85a95974efe736e3eb41e60cbf16f48
[MMusa39] <[musa.39@osu.edu]>
2022-03-29 16:56:36 -0400

    simplified solution

--- client/src/Home.js
+++ client/src/Home.js
@@ -64,12 +64,8 @@ const Home = (props) => {
   // Submit Ingredients
   const onSubmit = () => {
     let arr = "";
-    let goodInput = false;
     for (let ingredient in ingredients) {
-      if (ingredients[ingredient].text.trim().length !== 0) {
-        goodInput = true;
-      }
-      arr += ingredients[ingredient].text + ",";
+      arr += ingredients[ingredient].text.trim() + ",";
       //console.log(ingredients[ingredient].text);
     }
     // slice the last if it is ,
@@ -88,36 +84,32 @@ const Home = (props) => {
     }
     //navigate(`/search?q=${arr}`);
     //console.log(typeof arr)
-    if (!goodInput) {
-      r.push("No results found");
-    } else {
-      console.log(arr);
-      fetch("/search", {
-        method: "POST",
-        body: JSON.stringify({
-          content: arr,
-        }),
-      })
-        .then((response) =>
-          response.json().then((data) => ({ data, response }))
-        )
-        .then(({ data, response }) => {
-          //console.log(data["results"]["hits"]["hits"]);
-          for (let hit in data["results"]["hits"]["hits"]) {
-            let h = JSON.stringify(
-              data["results"]["hits"]["hits"][hit]["_source"]
-            );
 
-            r.push(h);
-          }
-          if (r.length === 0) {
-            r.push("No results found");
-          }
-          response = data;
-          setResults(r);
-          console.log(results);
-        });
-    }
+    console.log(arr);
+    fetch("/search", {
+      method: "POST",
+      body: JSON.stringify({
+        content: arr,
+      }),
+    })
+      .then((response) => response.json().then((data) => ({ data, response })))
+      .then(({ data, response }) => {
+        //console.log(data["results"]["hits"]["hits"]);
+        for (let hit in data["results"]["hits"]["hits"]) {
+          let h = JSON.stringify(
+            data["results"]["hits"]["hits"][hit]["_source"]
+          );
+
+          r.push(h);
+        }
+        if (r.length === 0) {
+          r.push("No results found");
+        }
+        response = data;
+        setResults(r);
+        console.log(results);
+      });
+
     console.log(results);
     console.log(typeof SampleRecipeData[0]);
     // console.log(typeof JSON.parse(results[0]))
